555fda04f153e6975a026b0f729542b2f3b68e48
Dyandres90 <fredyandresmarroquin@gmail.com>
2023-01-13 15:08:07 -0300

    add new URL frontend

--- src/main/java/com/portfolio/fam/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/fam/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "https://frontend-arg-programa.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
